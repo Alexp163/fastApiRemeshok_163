c71523a43664de25c152bcafa4974fecf43ca72b
alex163 <dr.p163@ya.ru>
2024-04-04 23:08:00 +0400

    Окончательный Комит перед отправкой проекта на хост

--- src/db.py
+++ src/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:1234@localhost"
+SQLALCHEMY_DATABASE_URI = "sqlite:///db.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 SessionLocal = sessionmaker(engine)
 Base = declarative_base()
